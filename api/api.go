07c3c39f1f0cfd5e749b8ddebb484c1d69779a5d
Merrill Lines <linesmerrill@gmail.com>
2019-06-25 18:12:48 -0700

    add product route and basic tests

--- api/api.go
+++ api/api.go
@@ -1,18 +1,36 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
+	"go.uber.org/zap"
+
 	"github.com/gorilla/mux"
 )
+
+type product struct {
+	ID          string  `json:"id"`
+	Name        string  `json:"name"`
+	Price       float64 `json:"price"`
+	Description string  `json:"description"`
+}
+
+// Init function to bring a new logger online
+func Init() {
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+}
 
 // Run starts the new mux router
 func Run() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homeHandler)
-	//r.HandleFunc("/products", ProductsHandler)
-	//r.HandleFunc("/articles", ArticlesHandler)
+	r.HandleFunc("/product/{id}", productsHandler).Methods("GET")
+	r.HandleFunc("/products", getProducts)
 	http.Handle("/", r)
 	log.Println("server running at localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
@@ -21,4 +39,36 @@ func Run() {
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	_, _ = w.Write([]byte(`{"health": "ok"}`))
+}
+
+func productsHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	thisProduct, err := getProduct(id)
+	if err != nil {
+		zap.NamedError("error getting product", err)
+		return
+	}
+
+	out, err := json.Marshal(thisProduct)
+	if err != nil {
+		zap.NamedError("error marshaling product", err)
+	}
+	_, _ = w.Write(out)
+}
+
+func getProduct(productID string) (product, error) {
+	file, err := ioutil.ReadFile(fmt.Sprintf("./test/products/%s", productID))
+	if err != nil {
+		zap.NamedError("error opening file", err)
+	}
+
+	myProduct := product{}
+	_ = json.Unmarshal(file, &myProduct)
+	return myProduct, nil
+}
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	_, _ = w.Write([]byte(`{}`))
 }
---
+++ api/api_test.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHomeHandler(t *testing.T) {
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	}
+
+	req := httptest.NewRequest("GET", "http://example.com/foo", nil)
+	w := httptest.NewRecorder()
+	handler(w, req)
+
+	resp := w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Println(resp.StatusCode)
+	fmt.Println(resp.Header.Get("Content-Type"))
+	fmt.Println(string(body))
+
+	assert.NotNil(t, body)
+
+}
